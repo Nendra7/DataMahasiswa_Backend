db6839a0b71529300b93a85e84b0cf503e15d99d
Nendra7 <irwannendra@gmail.com>
2025-10-26 08:38:39 +0700

    Add files via upload

--- routes/data_siswaRoutes.js
+++ routes/data_siswaRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const pelangganController = require('../controllers/data_siswaController');
 const data_siswaController = require('../controllers/data_siswaController');
 
 //routes
